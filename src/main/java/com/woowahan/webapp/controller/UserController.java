1ce6bc640d3c7663c8e1dd7aa180f4793de747f2
부은형 <boo1984@naver.com>
2018-01-26 11:32:40 +0900

    [ADD] User 목록 보여주기 구현
    User 목록 보여주는 html 과 User profile을 보여주는 html이 같아서
    User profile을 보여주는 html을 profile.html로 변경함

--- src/main/java/com/woowahan/webapp/controller/UserController.java
+++ src/main/java/com/woowahan/webapp/controller/UserController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/users")
 public class UserController {
@@ -19,7 +21,7 @@ public class UserController {
     @PostMapping("")
     public String create(User user) {
         userRepository.save(user);
-        return "redirect:/";
+        return "redirect:/users";
     }
 
     @GetMapping("/{id}")
@@ -27,6 +29,14 @@ public class UserController {
         User user = userRepository.findOne(id);
 
         model.addAttribute("user", user);
+
+        return "user/profile";
+    }
+
+    @GetMapping("")
+    public String showAllUsers(Model model) {
+        List<User> users = (List<User>) userRepository.findAll();
+        model.addAttribute("users", users);
 
         return "user/list";
     }
